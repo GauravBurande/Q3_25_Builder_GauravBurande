1a081e73a08b770cdff9d4dd35698a27ef7cfcd0
Gaurav <burandegaurav8899@gmail.com>
2025-07-03 19:11:42 +0530

    it's rug day!

---
+++ ts/cluster1/nft_metadata.ts
@@ -0,0 +1,46 @@
+import wallet from "./wallet/turbin3-wallet.json";
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
+import {
+  createGenericFile,
+  createSignerFromKeypair,
+  signerIdentity,
+} from "@metaplex-foundation/umi";
+import { irysUploader } from "@metaplex-foundation/umi-uploader-irys";
+
+// Create a devnet connection
+const umi = createUmi("https://api.devnet.solana.com");
+
+let keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(wallet));
+const signer = createSignerFromKeypair(umi, keypair);
+
+umi.use(irysUploader({ address: "https://devnet.irys.xyz/" }));
+umi.use(signerIdentity(signer));
+
+(async () => {
+  try {
+    // Follow this JSON structure
+    // https://docs.metaplex.com/programs/token-metadata/changelog/v1.0#json-structure
+    const image =
+      "https://devnet.irys.xyz/4X1orheMMgchWfHHZspFJGRjtWLYLig92SS6zJ3VnaDm";
+    const metadata = {
+      name: "alladin's rug",
+      symbol: "ADNR",
+      description: "This won't fly to the moon",
+      image,
+      attributes: [{ trait_type: "natural", value: "looks green" }],
+      properties: {
+        files: [
+          {
+            type: "image/png",
+            uri: image,
+          },
+        ],
+      },
+      creators: [],
+    };
+    const myUri = await umi.uploader.uploadJson(metadata);
+    console.log("Your metadata URI: ", myUri);
+  } catch (error) {
+    console.log("Oops.. Something went wrong", error);
+  }
+})();
